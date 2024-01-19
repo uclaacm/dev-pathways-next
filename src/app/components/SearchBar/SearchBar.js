a3fc1275a0de347751f5d786723b524cfcefbebf
maxgonick <maxwellgonick@g.ucla.edu>
2024-01-18 19:08:24 -0800

    add package-json

--- src/app/components/SearchBar/SearchBar.js
+++ src/app/components/SearchBar/SearchBar.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import './SearchBar.css';
 import Clickable from '../Clickable/Clickable';
-import line from 'src/app/components/SearchBar/line.svg';
-import search from 'src/app/components/SearchBar/SearchBar.js';
+import line from './line.svg';
+import search from "./search.svg"
 import TypingAnimation from '../TypingAnimation/TypingAnimation';
+import Image from 'next/image';
 
 const SearchBar = props => {
     const [showPlaceholder, setShowPlaceholder] = useState(true);
@@ -13,10 +14,12 @@ const SearchBar = props => {
         <div className="searchbar">
             <div className="search-icon">
                 <Clickable onClick={onSubmit}>
-                    <img src={search} alt="search icon" />
+                    {/* <img src={search} alt="search icon" /> */}
+                    <Image src={search} alt="search icon" width={100} height={100}/>
                 </Clickable>
             </div>
-            <img src={line} alt="line" />
+            {/* <img src={line} alt="line" /> */}
+            <Image src={line} alt="line" width={100} height={100}/>
             <div>
                 {showPlaceholder && !value && <label for="search">
                     What do you want to learn? <span id="animation"><TypingAnimation words={["html/css", "javascript", "react"]} /></span>
