f68eb6ff451def36e7785fb4aa5e7de62d035114
“ChiweiChen” <chiwei0705@gmail.com>
2024-02-25 23:34:52 -0800

    temp fix for generate button issue, seems like a CSS loading issue

--- src/app/components/SolidButton/SolidButton.js
+++ src/app/components/SolidButton/SolidButton.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './SolidButton.css';
-import Clickable from '../Clickable/Clickable';
+import React from "react";
+import "./SolidButton.css";
+import Clickable from "../Clickable/Clickable";
 
-const SolidButton = props => {
-    return (
-        <Clickable className={`solid-button ${props.className}`} onClick={props.onClick}>
-            <span className="text">{props.text}</span>
-        </Clickable>
-    );
-}
+const SolidButton = (props) => {
+  return (
+    <Clickable
+      className={`solid-button ${props.className}`}
+      onClick={props.onClick}
+    >
+      <span className="text">{props.text}</span>
+    </Clickable>
+  );
+};
 
 export default SolidButton;
